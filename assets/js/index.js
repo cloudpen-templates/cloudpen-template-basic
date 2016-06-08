4f36bcd7a7fbf2b8b1a2e6bd3165ff45cb7c3aa6
Gabriel Lantz <gabriel@Gabriel-Lantzs-iMac-9.local>
2016-06-08 22:22:53 +0200

    Editing template locally

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,19 +7,3 @@ Created:        28/04/2014
 Last change:    22/07/2014
 -------------------------------------------------------------------*/
 
-/* ===== Tooltips ===== */
-
-$('#tooltip').tooltip();
-
-/* ===== Feedback ===== */
-
-$('.feedback-author').hover (function() {
-    var quote = $(this).data('quote');
-    $('.feedback-author').removeClass("active");
-    $(this).addClass("active");
-    $('.feedback-quote').removeClass("show animated fadeIn");
-    $('.feedback-quote').addClass("hidden");
-    $('.feedback-quote' + quote).toggleClass("hidden show");
-    $('.feedback-quote' + quote).addClass("animated fadeIn");
-    return false;
-});
